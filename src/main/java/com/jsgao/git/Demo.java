70049f0fd3e1fd72dda93c53490c71ea205d0817
jsgao <2209285818@qq.com>
2021-09-23 10:18:45 +0800

    First Commit

---
+++ src/main/java/com/jsgao/git/Demo.java
@@ -0,0 +1,11 @@
+package com.jsgao.git;
+
+/**
+ * @Author jsgao
+ * @Date 2021/9/23 - 10:16
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
