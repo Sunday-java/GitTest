35fba23f02bb9730edcad1b45f97db32ca9cb24c
jsgao <2209285818@qq.com>
2021-09-23 10:40:42 +0800

    master first Commit

--- src/main/java/com/jsgao/git/Demo.java
+++ src/main/java/com/jsgao/git/Demo.java
@@ -12,5 +12,6 @@ public class Demo {
         System.out.println("Hello Git4");
         System.out.println("master");
         System.out.println("hot-fix");
+        System.out.println("master push");
     }
 }
