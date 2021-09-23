c88444c97a6157a27612e48c62d721db577b89d5
jsgao <2209285818@qq.com>
2021-09-23 10:20:06 +0800

    Second Commit

--- src/main/java/com/jsgao/git/Demo.java
+++ src/main/java/com/jsgao/git/Demo.java
@@ -7,5 +7,6 @@ package com.jsgao.git;
 public class Demo {
     public static void main(String[] args) {
         System.out.println("Hello Git");
+        System.out.println("Hello Git2");
     }
 }
